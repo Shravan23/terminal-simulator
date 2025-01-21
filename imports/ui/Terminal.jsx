d2f19eae0d3754a1efdbf8857e2bc1a07c6a5147
Shravan Kumar Bachu <bachs01@pfw.edu>
2025-01-21 00:34:30 -0500

    Fixed error of handling backspace and enter

--- imports/ui/Terminal.jsx
+++ imports/ui/Terminal.jsx
@@ -6,25 +6,32 @@ import 'xterm/css/xterm.css';
 const TerminalComponent = () => {
     const terminalRef = useRef(null);
     const term = useRef(null);
+    const inputBuffer = useRef('');
     useEffect(() => {
         term.current = new Terminal({
             cursorBlink: true,
         });
         term.current.open(terminalRef.current);
-
         term.current.onData((data) => {
-            if(data === '\r') {
-                term.current.write('\r\n'); // New line
-            } else {
-                term.current.write(data); // Echo input back to the terminal
+            if (data === '\r') {
+                term.current.write('\r\n');
+                inputBuffer.current = '';
+            } else if (data === '\x7f') {
+                if (inputBuffer.current.length > 0) {
+                    inputBuffer.current = inputBuffer.current.slice(0, -1);
+                    term.current.write('\b \b');
+                }
+            }
+            else {
+                inputBuffer.current += data;
+                term.current.write(data);
             }
         })
         term.current.write('Welcome to the xterm.js Terminal!\r\n');
         return () => {
-            // Cleanup terminal on unmount
             term.current.dispose();
         };
-    },[]);
+    }, []);
     return <div ref={terminalRef} style={{ height: '400px', width: '100%' }}></div>;
 };
 
